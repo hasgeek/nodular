851fff051a194e061cdd110e32d0f88fe2d60587
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-03-21 13:23:31 +0530

    Fix for SQLite3 foreign keys.

--- nodular/db.py
+++ nodular/db.py
@@ -31,3 +31,17 @@ from flask.ext.sqlalchemy import SQLAlchemy
 __all__ = ['db']
 
 db = SQLAlchemy()
+
+
+# To enable foreign key support in SQLite3
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from sqlite3 import Connection as SQLite3Connection
+
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
